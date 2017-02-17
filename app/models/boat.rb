f7ea41269934d86faab8b6f16446dd7ca4e390e3
nanyr <nanyramirez0@gmail.com>
2017-02-17 15:17:05 -0500

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,43 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    limit(5)
+  end
+
+  def self.dinghy
+    where("length<20")
+  end
+
+  def self.ship
+    where("length>20")
+  end
+
+  def self.last_three_alphabetically
+    order(name: :desc).limit(3)
+  end
+
+  def self.without_a_captain
+    where(captain: nil)
+  end
+
+  def self.sailboats
+    joins(:boat_classifications, :classifications)
+    .where("classifications.name = 'Sailboat'")
+    .group("boats.name")
+    .order("boats.id")
+  end
+
+  def self.with_three_classifications
+    joins(:classifications)
+    .group("boats.id")
+    .having("count(*) =3").select(:name)
+  end
+
+  def self.longest
+    order("length DESC").first 
+  end
+
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,51 @@
+require 'pry'
+
 class Captain < ActiveRecord::Base
   has_many :boats
+
+
+  def self.catamaran_operators
+    # go through boats and from there find relationship to classifications, relying on the boat class defining its relationship to classifications (captain doesn't know about classficiations).same as {boats: :classifications}
+    get_boat_type("Catamaran")
+    # with_classifications
+    # .where(classifications: {name: 'Catamaran'})
+  end
+
+  def self.with_classifications
+    joins(:boats => :classifications)
+  end
+
+  def self.sailors
+    # can also use uniq nstead of group
+    with_classifications
+    .group("captains.name")
+    .where(classifications: {name: 'Sailboat'})
+    .select(name)
+  end
+
+  def self.get_boat_type(type)
+    with_classifications
+    .where(classifications: {name: type}).uniq
+  end
+
+  def self.motorboaters
+    get_boat_type("Motorboat")
+  end
+
+
+  def self.talented_seamen
+    mb= motorboaters.pluck(:id)
+    sb=sailors.pluck(:id)
+    where(id: (mb & sb))
+  end
+
+  def self.non_sailors
+    sailors=self.sailors.pluck(:name)
+    all_sailors=self.all.pluck(:name)
+    where(name: (all_sailors - sailors))
+
+  end
+
+
+
 end
